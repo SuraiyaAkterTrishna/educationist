15b21146701394612de5989ab22ccd554f9f64af
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-19 21:26:56 +0600

    basic installation done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Hello From Educationist</h1>
+      <button class="btn btn-primary">Button</button>
     </div>
   );
 }
